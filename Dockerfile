ec89e366edecdac3570175bc3d68e24e71edd70c
William Finnigan <wjafinnigan@gmail.com>
2021-05-18 11:50:52 +0100

    fixed api

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,7 @@ FROM continuumio/miniconda3:4.8.2
 
 RUN conda install -c conda-forge jsonnet
 
-RUN pip install bio-embeddings-allennlp==0.9.2
-RUN pip install boto3==1.17.74
-RUN pip install botocore==1.20.74
-RUN pip install bio-embeddings==0.1.6
+RUN pip install bio-embeddings[seqvec]
 RUN pip install fastapi uvicorn python-multipart
 
 EXPOSE 80
--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import os
 import subprocess as sp
 import shutil
 from typing import Optional
-from bio_embeddings.embed import SeqVecEmbedder
+#from bio_embeddings.embed import SeqVecEmbedder
+from pydantic import BaseModel
+
+class Sequence(BaseModel):
+    seq: str
 
 app = FastAPI()
 
@@ -12,10 +16,11 @@ def read_root():
     return {"Hello": "World"}
 
 @app.post("/seqvec")
-def seqvec(sequence: str):
+def seqvec(seq: Sequence):
+    seq_str = seq.seq
     try:
         embedder = SeqVecEmbedder()
-        em_per_res = embedder.embed(sequence)
+        em_per_res = embedder.embed(seq_str)
         em_per_prot = embedder.reduce_per_protein(em_per_res)
         return em_per_prot
     except:
