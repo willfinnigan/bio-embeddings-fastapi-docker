ea1ff15eceabe365a31e1ed961c38160a69733e3
William Finnigan <wjafinnigan@gmail.com>
2021-05-18 17:36:03 +0100

    fixed api

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import shutil
 from typing import Optional
 from bio_embeddings.embed import SeqVecEmbedder
 from pydantic import BaseModel
+import json
+from typing import List, Optional
 
 class Sequence(BaseModel):
     seq: str
+
+class Embedding(BaseModel):
+    seqvec: List[float] = []
 
 app = FastAPI()
 
@@ -15,13 +20,19 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/seqvec")
+@app.post("/seqvec", response_model=Embedding)
 def seqvec(seq: Sequence):
-    seq_str = seq.seq
+    seq_str = dict(seq)['seq']
     try:
         embedder = SeqVecEmbedder()
         em_per_res = embedder.embed(seq_str)
         em_per_prot = embedder.reduce_per_protein(em_per_res)
-        return em_per_prot
+        print(list(em_per_prot))
+        return {"seqvec": list(em_per_prot)}
+
     except:
         return 'Error running embedding'
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=80)
